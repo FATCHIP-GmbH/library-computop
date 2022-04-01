104d9c564bc98b2e5c9918fbf4b261d254c8726f
Stefan Mueller <stefstar@stefstar.de>
2022-04-01 09:20:44 +0200

    0085011: deactivate and remove afterpay installments

--- lib/FCSPayment/CTPaymentMethods.php
+++ lib/FCSPayment/CTPaymentMethods.php
@@ -182,7 +182,7 @@ class CTPaymentMethods
                 'className' => 'Afterpay',
                 //'countries' => ['DE', 'NL', 'DK', 'FI', 'SE', 'NO'],
             ],
-            [
+/*            [
                 'name' => 'fatchip_firstcash_afterpay_installment',
                 'shortname' => 'AfterpayInstallment',
                 'description' => 'First Cash Solution Afterpay Ratenzahlung',
@@ -192,5 +192,6 @@ class CTPaymentMethods
                 'className' => 'Afterpay',
                 //'countries' => ['DE', 'NL', 'DK', 'FI', 'SE', 'NO'],
             ],
+*/
         ];
 }
